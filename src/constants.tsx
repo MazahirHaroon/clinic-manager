7900c9a781c89ab68095907091418986c6c69030
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-02-26 17:27:44 +0530

    created a new Select component

---
+++ src/constants.tsx
@@ -0,0 +1,18 @@
+export const HELP_EMAIL: string = 'help@example.com';
+export const DEPARTMENT_LIST: { key: number; value: string; label: string }[] = [
+    {
+        key: 1,
+        value: 'general-medicine',
+        label: 'General Medicine',
+    },
+    {
+        key: 2,
+        value: 'oncology',
+        label: 'Oncology',
+    },
+    {
+        key: 3,
+        value: 'cardiology',
+        label: 'Cardiology',
+    },
+];
