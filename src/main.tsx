f8d1dea0b1ded96db537e3a4eca6ab5136b110b4
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-02-28 00:31:18 +0530

    formatting changes (set prettier as default to catch this while creation)

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
-import './index.css'
+import './index.css';
 
 import Home from '@components/Home';
 import Signup from '@components/Signup';
 import NotFound from '@components/common/NotFound';
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <Home />,
-  errorElement: <NotFound />
-}, {
-  path: "/Signup",
-  element: <Signup />
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/Signup',
+    element: <Signup />,
+  },
 ]);
 
 const rootElement = document.getElementById('root');
@@ -27,5 +29,5 @@ if (rootElement) {
     </StrictMode>
   );
 } else {
-  console.error("Root element not found");
+  console.error('Root element not found');
 }
