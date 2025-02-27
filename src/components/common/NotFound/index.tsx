31b8799e5eae0823505a569c38fc1afed9a5eae9
Mazahir Haroon <mazahirharoon@gmail.com>
2025-02-27 23:43:16 +0530

    added Home links to the NotFound and minor change to styles and formatting.

--- src/components/common/NotFound/index.tsx
+++ src/components/common/NotFound/index.tsx
@@ -1,20 +1,23 @@
-import Logo from "@common-components/Logo";
-import PrimaryButton from "@common-components/Button/primary";
-import "./index.css";
+import { Link } from 'react-router';
+
+import Logo from '@common-components/Logo';
+import PrimaryButton from '@common-components/Button/primary';
+
+import './index.css';
 
 const NotFound = () => {
     return (
-        <div className="not-found-container">
-            <div className="logo-container">
-                <Logo src="/clinic-manager-logo-and-heading.png" alt="Clinic Manager" />
+        <div className='not-found-container'>
+            <div className='logo-container'>
+                <Logo src='/clinic-manager-logo-and-heading.png' alt='Clinic Manager' />
             </div>
-            <h1 className="not-found-title">404</h1>
-            <p className="not-found-message">{"Oops! The page you're looking for doesn't exist."}</p>
-            <div className="button-group">
-                <div>
-                    <PrimaryButton content="Go Home" />
-                </div>
-            </div>
+            <h1 className='not-found-title'>404</h1>
+            <p className='not-found-message'>
+                {"Oops! The page you're looking for doesn't exist."}
+            </p>
+            <Link to="/">
+                <PrimaryButton content={"Let's Go Home"} />
+            </Link>
         </div>
     );
 };
