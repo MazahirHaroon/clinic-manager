e6e659c4b2854840010d564af899d4c8ffa4c4a9
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-02-28 00:36:18 +0530

    added links to Signup page from Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,30 +1,33 @@
+import { Link } from 'react-router';
+
 import Logo from '../common/Logo';
 import PrimaryButton from '@common-components/Button/primary';
+
 import './index.css';
 
 const Home = () => {
-    return (
-        <div className='home-container'>
-            <div className='content'>
-                <Logo
-                    src={'public/clinic-manager-logo-and-heading.png'}
-                    alt={'Clinic Manager'}
-                />
-                <h1 className='home-title'>Welcome to Clinic Manager</h1>
-                <p className='home-subtitle'>
-                    Your digital assistant for seamless clinic management.
-                </p>
-                <div className='home-buttons'>
-                    <div>
-                        <PrimaryButton content='Sign Up' type='button' />
-                    </div>
-                    <div>
-                        <PrimaryButton content='Login' type='button' />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='home-container'>
+      <div className='content'>
+        <Logo
+          src={'public/clinic-manager-logo-and-heading.png'}
+          alt={'Clinic Manager'}
+        />
+        <h1 className='home-title'>Welcome to Clinic Manager</h1>
+        <p className='home-subtitle'>
+          Your digital assistant for seamless clinic management.
+        </p>
+        <div className='home-buttons'>
+          <Link to='./Signup'>
+            <PrimaryButton content='Sign Up' type='button' />
+          </Link>
+          <div>
+            <PrimaryButton content='Login' type='button' />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
