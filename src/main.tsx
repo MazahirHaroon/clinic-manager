c15ce23fada460eebacf5cabe3a6c7999953deb2
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-02-27 21:26:50 +0530

    setup routes for Home and Signup Page.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+import './index.css'
+
+import Home from '@components/Home';
+import Signup from '@components/Signup';
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Home />
+}, {
+  path: "/Signup",
+  element: <Signup />
+}
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
