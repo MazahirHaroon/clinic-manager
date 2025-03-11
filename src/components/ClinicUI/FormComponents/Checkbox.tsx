c3dd4c7ad986cd155fa9966c4a914c2d48d2c4ef
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-11 08:53:08 +0530

    Used Tailwind in all the Form Components and deleted the css file.

--- src/components/ClinicUI/FormComponents/Checkbox.tsx
+++ src/components/ClinicUI/FormComponents/Checkbox.tsx
@@ -1,27 +1,27 @@
 import { useField } from 'formik';
-// import "./index.css";
 
 interface InputProps {
-    label: string;
-    name: string;
+  label: string;
+  name: string;
 }
 
 const CheckBox = ({ label, ...props }: InputProps) => {
-    const [field, meta] = useField(props);
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
 
-    return (
-        <div className='input-container'>
-            <input
-                type="checkbox"
-                {...field}
-                {...props}
-            />
-            <span>{label}</span>
-            {meta.touched && meta.error ? (
-                <p className='error-message'>{meta.error}</p>
-            ) : null}
-        </div>
-    );
+  return (
+    <div className='flex items-center gap-2'>
+      <input
+        type='checkbox'
+        {...field}
+        {...props}
+        className='w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-2 focus:ring-blue-500'
+      />
+      <span className='text-gray-700'>{label}</span>
+      {meta.touched && meta.error ? (
+        <p className='text-red-500 text-sm'>{meta.error}</p>
+      ) : null}
+    </div>
+  );
 };
 
 export default CheckBox;
