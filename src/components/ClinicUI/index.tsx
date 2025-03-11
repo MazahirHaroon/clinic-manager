8a860e058232b8721d4e58a4404a5d74753282fa
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-11 08:57:20 +0530

    updated the imports as per Form Component renaming.

--- src/components/ClinicUI/index.tsx
+++ src/components/ClinicUI/index.tsx
@@ -1,6 +1,6 @@
-import Input from './FormComponents/input';
-import Select from './FormComponents/select';
-import CheckBox from './FormComponents/checkbox';
-import PrimaryButton from './Button/primary';
+import Input from './FormComponents/Input';
+import Select from './FormComponents/Select';
+import CheckBox from './FormComponents/Checkbox';
+import PrimaryButton from './Button/Primary';
 
 export { CheckBox, Input, PrimaryButton, Select };
