651819f5fe66a19099271b4564e8bf53eb45e3b6
Mazahir Haroon <mazahirharoon@gmail.com>
2025-03-17 15:03:17 +0530

    build: configure path aliases for appwrite directory

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       '@clinic-ui': '/src/components/ClinicUI/index',
       '@components': '/src/components',
       '@conf': '/src/conf',
+      '@appwrite': '/src/appwrite',
     },
   },
 });
