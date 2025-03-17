f665c74867c677e71d7ef1a0ec9022084cccabca
Mazahir Haroon <mazahirharoon@gmail.com>
2025-03-17 16:18:16 +0530

    feat: integrate user signup with database service

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,27 @@
-import authService from '@appwrite/auth';
-
+import { authService, databaseService } from '@appwrite';
+import { omitKeyValues } from '@utils/common';
 import { SignUpFormValues } from '@constants/auth';
 
 export const handleSignup = async (values: SignUpFormValues) => {
-  await authService.createAccount(
-    values.email,
-    values.password,
-    `${values.firstName} ${values.lastName}`
-  );
+  try {
+    const userAccount = await authService.createAccount(
+      values.email,
+      values.password,
+      `${values.firstName} ${values.lastName}`
+    );
+
+    const userProps = omitKeyValues(
+      ['acceptedTos', 'password', 'confirmPassword'],
+      values
+    );
+
+    return await databaseService.createUser(userAccount.$id, userProps);
+  } catch (error) {
+    const errorSource = 'Utils ::  Auth :: handleSignup()';
+    if (error instanceof Error) {
+      throw new Error(`${errorSource} :: ${error.message}`);
+    } else {
+      throw new Error(errorSource);
+    }
+  }
 };
