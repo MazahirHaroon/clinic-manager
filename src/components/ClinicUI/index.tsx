dacae4b31b4f89ea68cbd25f97273f1d6e62a1c2
Mazahir Haroon <mazahirharoon@gmail.com>
2025-03-20 17:17:40 +0530

    feat: created the navbar component

--- src/components/ClinicUI/index.tsx
+++ src/components/ClinicUI/index.tsx
@@ -1,6 +1,7 @@
+import Navbar from './Navbar';
 import Input from './FormComponents/Input';
 import Select from './FormComponents/Select';
 import CheckBox from './FormComponents/Checkbox';
 import PrimaryButton from './Button/Primary';
 
-export { CheckBox, Input, PrimaryButton, Select };
+export { Navbar, CheckBox, Input, PrimaryButton, Select };
