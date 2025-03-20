07d909d59bd28042da917fb9bf5a8d0e5e0bd043
Mazahir Haroon <mazahirharoon@gmail.com>
2025-03-20 17:28:48 +0530

    style: improved the mobile responsiveness

--- src/components/ClinicUI/Navbar.tsx
+++ src/components/ClinicUI/Navbar.tsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router';
 import Logo from '@components/Logo';
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center min-h-24 px-10 pr-30 bg-light-background'>
+    <nav className='flex justify-center flex-col lg:flex-row lg:justify-between items-center min-h-24 flex-wrap px-10 pr-10 bg-light-background'>
       <Logo src={'heading-clinic-manager.png'} className='w-48' />
-      <div className='flex gap-6 text-lg'>
+      <div className='flex gap-6 text-base'>
         <Link className='text-primary-light hover:text-primary-dark' to='/'>
           Home
         </Link>
