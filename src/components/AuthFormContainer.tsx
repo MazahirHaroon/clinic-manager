225e0f507caae5b7ce8e964d8fb2e173f5346192
Mazahir Haroon <mazahirharoon@gmail.com>
2025-03-20 15:44:53 +0530

    style: provided style values from AuthFormContainer for the Logo

--- src/components/AuthFormContainer.tsx
+++ src/components/AuthFormContainer.tsx
@@ -11,10 +11,7 @@ const AuthFormContainer = ({ title, children }: AuthFormContainerProps) => {
   return (
     <div className='flex justify-center items-center min-h-screen bg-primary-light'>
       <div className='flex flex-col w-full max-w-xl px-12 pb-6 pt-1 mt-6 bg-white shadow-lg rounded-3xl'>
-        <Logo
-          src={'public/clinic-manager-logo-and-heading.png'}
-          alt={'Clinic Manager'}
-        />
+        <Logo className='w-[50%]' />
         <h2 className='text-2xl font-semibold text-primary-dark text-center mb-4'>
           {title}
         </h2>
