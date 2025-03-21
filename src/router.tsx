320404bf2784300495fe53f69bc0aa439d52d8b4
Mazahir Haroon <mazahirharoon@gmail.com>
2025-03-21 08:10:54 +0530

    refactor: use createRoutesFromElements syntax for setting routes.

--- src/router.tsx
+++ src/router.tsx
@@ -1,34 +1,22 @@
-import { createBrowserRouter } from 'react-router';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router';
 import { Welcome, NotFound, SignUp, Login, Profile, Dashboard } from '@pages';
 import { Layout } from '@clinic-ui';
 
-export const router = createBrowserRouter([
-  {
-    path: '/welcome',
-    element: <Welcome />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: '/signup',
-    element: <SignUp />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: '',
-        element: <Dashboard />,
-      },
-      {
-        path: '/profile',
-        element: <Profile />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Dashboard />} errorElement={<NotFound />} />
+        <Route path='/profile' element={<Profile />} />
+      </Route>
+      <Route path='/welcome' element={<Welcome />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='*' element={<NotFound />} />
+    </>
+  )
+);
