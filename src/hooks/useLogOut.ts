b7aee55c5f6c088a45434bb54361c026a97f8e40
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-23 18:26:43 +0530

    feat: create useLogout hook to handle logging out the user

---
+++ src/hooks/useLogOut.ts
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router';
+
+import { authService } from '@appwrite';
+
+export const useLogOut = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const currentUser = await authService.getCurrentUser();
+      console.log(currentUser);
+      if (currentUser) {
+        await authService.logOut();
+      }
+      void navigate('/welcome');
+    } catch (error) {
+      const errorSource = 'Hook ::  useLogOut :: handleLogout()';
+      if (error instanceof Error) {
+        throw new Error(`${errorSource} :: ${error.message}`);
+      } else {
+        throw new Error(errorSource);
+      }
+    }
+  };
+  return { handleLogout };
+};
