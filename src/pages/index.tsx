982270edd348cacd70068d1a7bd6b798b7fd5cdc
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-06-15 17:30:55 +0530

    refactor: move profile page out of dashboard directory and update corresponding routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,6 @@ import NotFound from './NotFound';
 import SignUp from './SignUp';
 import Login from './Login';
 import Dashboard from './Dashboard';
-import Profile from './Dashboard/Profile';
+import Profile from './Profile';
 
 export { Welcome, NotFound, SignUp, Login, Dashboard, Profile };
